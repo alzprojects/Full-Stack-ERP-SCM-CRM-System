540906f21055b2b14ecbfb5d51c340e0c7a79da1
SeifMehelmy <161532109+SeifMehelmy@users.noreply.github.com>
2024-04-19 13:52:41 -0400

    Update suppliers_page.php

--- suppliers_page.php
+++ suppliers_page.php
@@ -31,7 +31,8 @@ if(isset($_GET['userId']))
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Supplier Page</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="suppliersstyle.css">
+
 <style>
     .welcome {
         font-size: 18px;
